e84b08c3e2692a2bf85d33cd8cdfe3708042ca1c
rijwan-uddin <rijwan35-653@diu.edu.bd>
2024-04-26 16:19:51 +0600

    changed

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Lexiquest',
       theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.deepPurple,
